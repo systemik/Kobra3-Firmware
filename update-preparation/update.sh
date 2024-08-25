4b0755cf4f441818254992d215d28787ec418613
systemik <sebastien2@laverriere.org>
2024-08-25 17:18:46 +0200

    Chmod the start script

--- update-preparation/update.sh
+++ update-preparation/update.sh
@@ -2,5 +2,9 @@ echo "usb_adb_en" > /tmp/.usb_config
 cp -r /useremain/update_swu/openssh /ac_lib/lib/openssh
 cp -r /useremain/update_swu/openssh /useremain/openssh
 chmod +x /ac_lib/lib/openssh/sshd_start.sh
-chmod +x /useremain/update_swu/openssh/sshd_start.sh
+chmod +x /useremain//openssh/sshd_start.sh
+chmod +x /ac_lib/lib/openssh/bin/*
+chmod +x /ac_lib/lib/openssh/sbin/*
+chmod +x /useremain/openssh/bin/*
+chmod +x /useremain/openssh/sbin/*
 /useremain/openssh/sshd_start.sh
