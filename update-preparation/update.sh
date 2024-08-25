7cda582daa66ff997f77cf4359812f6f069243ec
systemik <sebastien2@laverriere.org>
2024-08-25 17:01:24 +0200

    Update ssh config

--- update-preparation/update.sh
+++ update-preparation/update.sh
@@ -1,3 +1,4 @@
 echo "usb_adb_en" > /tmp/.usb_config
-cp -r /useremain/openssh /ac_lib/lib/openssh
+cp -r /useremain/update_swu/openssh /ac_lib/lib/openssh
+cp -r /useremain/update_swu/openssh /useremain/openssh
 /useremain/openssh/sshd_start.sh
